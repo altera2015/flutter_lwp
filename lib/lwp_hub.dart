f8f3469d5e3eac2e3cbdf33d6a62226eee5a9253
Ron Bessems <rbessems@gmail.com>
2021-07-05 16:40:58 -0400

    Adding dartdoc generated docs

--- lib/lwp_hub.dart
+++ lib/lwp_hub.dart
@@ -2,33 +2,73 @@ part of flutter_lwp;
 
 typedef Future<void> IHubSendFunction(Message msg);
 
+/// The main class used to discover hubs.
 abstract class IHubScanner {
   IHubScanner();
 
+  /// dispose should be called when done with the scanner.
+  /// this will also destroy all hubs attached.
   void dispose();
+
+  /// starts scanning for the hub, depending on the backend
+  /// implementation. Currently this scans for Bluetooth
+  /// hubs.
   void startScanning();
+
+  /// Stop scanning for Hubs.
   void stopScanning();
+
+  /// This stream is updated whenever the list of hubs
+  /// changes.
   Stream<HubList> get stream;
+
+  /// The hub list.
+  /// This is not a copy of the list so don't modify it.
   HubList get list;
 
+  /// IHubScanner factory. Used to instantiate an
+  /// object of IHubScanner depending on what platform
+  /// it is running on.
   factory IHubScanner.factory() {
     // only one transport implemented currently.
     return flutter_blue_transport.HubScanner();
   }
 }
 
+/// HubState contains the state of the hub
+/// This includes which peripherals are attached
+/// and if the hub is currently connected.
 class HubState {
+  /// map of #Peripherals by port id.
   Map<int, Peripheral> peripherals = {};
+
+  /// connected is true when the hub is
+  /// connected.
   bool connected = false;
 }
 
+/// The main hub interaction class.
 abstract class IHub {
+  HubState _hubState = HubState();
+  StreamSubscription<Message>? _messageStreamSubscription;
+  StreamController<HubState> _hubStateStreamController = StreamController.broadcast();
+
+  /// transmit places the message on the wire immediately
+  /// this bypasses the transaction mechanism completely.
+  /// implemented in the backend.
   Future<void> transmit(Message msg);
+
+  /// returns the name of the hub
   String get name;
+
+  /// Initiates the connection to the hub.
   Future<bool> connect();
 
-  HubState _hubState = HubState();
+  /// Broadcast stream of messages coming from the hub hardware.
+  Stream<Message> get stream;
 
+  /// Dispose should be called when you are finished with a hub.
+  /// it is automatically called when IHubScanner is disposed.
   void dispose() {
     _messageStreamSubscription?.cancel();
     _messageStreamSubscription = null;
@@ -40,23 +80,22 @@ abstract class IHub {
     _hubStateStreamController.close();
   }
 
-  Stream<Message> get stream;
-
-  StreamSubscription<Message>? _messageStreamSubscription;
-  StreamController<HubState> _hubStateStreamController = StreamController.broadcast();
-
+  /// Stream to listen to hub state changes
   Stream<HubState> get hubStateStream {
     return _hubStateStreamController.stream;
   }
 
+  /// Access the hub state.
   HubState get hubState {
     return _hubState;
   }
 
+  /// access a peripheral directly.
   Peripheral? peripheral(int port) {
     return _hubState.peripherals[port];
   }
 
+  /// internal function called from backend. Don't use.
   void postConnect() {
     _messageStreamSubscription = stream.listen((msg) async {
       if (msg is HubAttachedIOMessage) {
@@ -104,12 +143,18 @@ abstract class IHub {
   }
 
   Queue<Transaction> _transactions = Queue<Transaction>();
+
+  /// Messages to the hub tend to be request response patterns
+  /// queue takes a #Transaction object that hides the complications
+  /// from the user.
   Future queue(Transaction transaction) {
     _transactions.addLast(transaction);
     _processTransactions();
     return transaction.future;
   }
 
+  /// Sends a message to he hub without waiting for a response. It is
+  /// still queued in the transaction system waiting for a slot.
   Future<void> send(Message message) {
     return queue(Transaction<void>(callback: (send, stream) async {
       await send(message);
@@ -117,6 +162,7 @@ abstract class IHub {
     }));
   }
 
+  /// Disconnect from the Hub.
   Future<void> disconnect() async {
     _messageStreamSubscription?.cancel();
     _messageStreamSubscription = null;
@@ -129,4 +175,5 @@ abstract class IHub {
   }
 }
 
+/// List of Hubs.
 typedef HubList = List<IHub>;
